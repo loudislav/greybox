f3f11ca5bbd46a4ec7100ac1debdbfdf1f7bee82
Jakub Szymsza <jakub@szymsza.cz>
2023-12-03 18:43:28 +0100

    New EuroSDC instances config

---
+++ env.eurosdc-dev.ts
@@ -0,0 +1,9 @@
+module.exports = {
+  MODE: 'development',
+  ORGANIZER: 'eurosdc',
+  BASE_ROUTE: '/',
+  API_URL: 'https://api.debata21.cz/',
+  BUILD_DIR: 'eurosdc_dev',
+  STAGE: 'eurosdc-local',
+  BUGSNAG_KEY: null,
+};
---
+++ env.eurosdc-prod.ts
@@ -0,0 +1,9 @@
+module.exports = {
+  MODE: 'production',
+  ORGANIZER: 'eurosdc',
+  BASE_ROUTE: '/',
+  API_URL: 'https://api-prod.debata21.cz/',
+  BUILD_DIR: 'eurosdc',
+  STAGE: 'eurosdc',
+  BUGSNAG_KEY: 'c5988a8332d9e7594c4c5986484f50a8',
+};
